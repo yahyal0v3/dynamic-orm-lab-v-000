71ddc96ab1a525d3cd141c61da2a9ed7adbf3151
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2017-12-05 18:11:21 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -45,11 +45,11 @@ class InteractiveRecord
   end
 
   def self.find_by_name(name)
-    binding.pry
     DB[:conn].execute("SELECT * FROM #{self.table_name} WHERE '#{name}' = '#{name}'")
   end
 
   def self.find_by(attr)
+    binding.pry
     DB[:conn].execute("SELECT * FROM #{self.table_name} WHERE '#{attr}' = '#{attr}'")
   end
 
